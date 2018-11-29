95f613f31cf14ee4b36ed73b7e73bc760047551a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-29 09:04:24 +0000

    Deploy simonagren/simonagren.github.io to github.com/simonagren/simonagren.github.io.git:master

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-869cc0ffaa140286672b.js","d7367ecaf173b1d380cccb4e68e5b71a"],["/commons-d5b7da6030ae32e256fb.js","68f28e2a84770c182684640dfae68475"],["/index.html","486802f8fb86785716c3edfb3073b32e"],["/manifest.webmanifest","2f07bcb077eb380ce4d3d5660d8fcfef"],["/offline-plugin-app-shell-fallback/index.html","226fa66fa5f4652e1f44026f09f99618"]];
+var precacheConfig = [["/app-869cc0ffaa140286672b.js","d7367ecaf173b1d380cccb4e68e5b71a"],["/commons-d5b7da6030ae32e256fb.js","68f28e2a84770c182684640dfae68475"],["/index.html","07f7cc11dd8efd8bd171144e0179851a"],["/manifest.webmanifest","2f07bcb077eb380ce4d3d5660d8fcfef"],["/offline-plugin-app-shell-fallback/index.html","44cf122ec9b9d37673a0f33123431076"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
